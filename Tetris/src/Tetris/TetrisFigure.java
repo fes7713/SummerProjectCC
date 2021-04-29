681627314670b1ba4ec92b291459586f856d16dd
miyata <aaxyz0120@gmail.com>
2021-04-29 19:50:16 +0900

    commit1

--- Tetris/src/Tetris/TetrisFigure.java
+++ Tetris/src/Tetris/TetrisFigure.java
@@ -2,8 +2,78 @@ package Tetris;
 
 // Use BlockType.java and implement all of Tetris block type.
 // Use switch statement if you want.
-public class TetrisFigure extends Figure{
-    public TetrisFigure(Game game) {
+public class TetrisFigure extends Figure
+{
+    public TetrisFigure(Game game) 
+    {
         super(game);
+        
+    }
+    
+    public void figI(Game game)
+    {
+        boolean[][] fig = {
+                {false, true, false},
+                {false, true, false},
+                {false, true, false},
+                {false, true, false}
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figJ(Game game)
+    {
+        boolean[][] fig = {
+                {false, true, false},
+                {false, true, false},
+                {true, true, false}
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figL(Game game)
+    {
+        boolean[][] fig = {
+                {false, true, false},
+                {false, true, false},
+                {false, true, true}
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figS(Game game)
+    {
+        boolean[][] fig = {
+                {false, true, true},
+                {true, true, false},
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figZ(Game game)
+    {
+        boolean[][] fig = {
+                {true, true, false},
+                {false, true, true},
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figT(Game game)
+    {
+        boolean[][] fig = {
+                {true, true, true},
+                {false, true, false},
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
+    }
+    
+    public void figO(Game game)
+    {
+        boolean[][] fig = {
+                {true, true, false},
+                {true, true, false},
+        };
+        game.add(new Figure(game, fig, 4, 0, true));
     }
 }
