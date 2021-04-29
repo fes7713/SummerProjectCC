adf615a7c9d761f345ce00f70fe5622b36b8071b
Keita <tun05036@temple.edu>
2021-04-29 10:50:40 +0900

    Tetris Update

--- Tetris/src/Tetris/TetrisFigure.java
+++ Tetris/src/Tetris/TetrisFigure.java
@@ -1,6 +1,9 @@
 package Tetris;
 
 // Use BlockType.java and implement all of Tetris block type.
-// You may inherit figure class. or use has-a relationship of figure.
-public class TetrisFigure {
+// Use switch statement if you want.
+public class TetrisFigure extends Figure{
+    public TetrisFigure(Game game) {
+        super(game);
+    }
 }
